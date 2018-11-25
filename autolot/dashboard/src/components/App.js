748a676d91e5c21e76d86e071cb187186aa9fd20
ShainaR <shainaread@gmail.com>
2018-11-25 14:15:07 -0600

    testing js changes

--- autolot/dashboard/src/components/App.js
+++ autolot/dashboard/src/components/App.js
@@ -3,8 +3,13 @@ import ReactDOM from "react-dom";
 import GateDataProvider from "./GateDataProvider";
 
 const App = () => (
-  <GateDataProvider endpoint="/api/gate/" 
-                render={data => <Table data={data} />} />
+//    render() {
+//        return (
+//            <Header />
+//        )
+//    }
+//  <GateDataProvider endpoint="/api/gate/"
+//                render={data => <Table data={data} />} />
 );
 
 const wrapper = document.getElementById("app");
